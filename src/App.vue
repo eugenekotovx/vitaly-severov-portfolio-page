546e56ea597ab9e3d7ccfe7d668e0a24cccb4244
Eugene Kotov <mrkev2112@mail.ru>
2020-10-25 00:19:55 +0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <router-view/>
+  </div>
+</template>
+
+<script type="text/javascript">
+import NavBar from "@/components/NavBar"
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: "Open Sans Regular";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+@font-face {
+  font-family: "Open Sans Regular";
+  src: local("Merienda"),
+   url(./assets/fonts/OpenSans-Regular.ttf) format("truetype");
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div id="nav" class="nav">
+    <router-link class="page-label" to="/">VITALY SEVEROV</router-link>
+    <div class="menu">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css" scoped >
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
